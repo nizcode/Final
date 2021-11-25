6a751d5f2e4786a34013b366ed638617f9badba3
nizcode <neilfa99@gmail.com>
2021-11-25 15:39:15 +0000

    next step - NLP

---
+++ rough_work.R
@@ -0,0 +1,22 @@
+#final proj rough work
+```{r}
+df1$pgrp<-cut(df1$points,c(80,87,93,100),labels = c('low','medium','high'))
+pvc <- table(data.frame(df1$country,df1$pgrp))
+pvc <- addmargins(pvc)
+pvcH <- data.frame(pvc[,'high']/pvc[,'Sum'])
+pvcH$country<-rownames(pvcH)
+colnames(pvcH)[1] <- "highPerc"
+
+library(maps)
+library(gapminder)
+library(countrycode)
+#library(countrycode,lib.loc = "/home/neilfa/R/x86_64-pc-linux-gnu-library/4.1")
+world_map <- map_data("world")
+wine_map <- left_join(pvcH, mosaicData::Countries, 
+                      by = c("country" = "gapminder"))
+wine_map <- left_join(wine_map, world_map, by = c("maps" = "region"))
+
+as.factor(wine_map$country) %>% levels()
+ggplot(wine_map, aes(x = long, y = lat, group = group)) +
+  geom_polygon(aes(fill = highPerc), color = "white") +
+  scale_fill_viridis_c(option = "C")
